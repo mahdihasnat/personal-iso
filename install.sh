87c69dfd084c48e0ff8eb5a670bf42518088cf5c
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2024-01-20 12:06:32 +0600

    Allow parallel processing in install.sh

--- install.sh
+++ install.sh
@@ -4,14 +4,38 @@ set -eux
 
 shdir="scripts"
 
-for bashfile in $(cat $shdir/run_order.txt); do
-    echo -e "\nExecuting $bashfile ...\n"
-    bash "$shdir/$bashfile" &
-    # Store the background process ID in an array for later checking
-    pid_array+=($!)
-done
+run_scripts() {
+    mode=$1
+    scripts=$2
 
-# Wait for all background processes to finish and check their exit statuses
-for pid in "${pid_array[@]}"; do
-    wait "$pid" || exit $?
+    if [ "$mode" == "foreground" ]; then
+        for bashfile in "${scripts[@]}"; do
+			echo -e "\nExecuting $bashfile ...\n"
+    		bash "$shdir/$bashfile"
+        done
+    elif [ "$mode" == "background" ]; then
+		pid_array=()
+        for bashfile in "${scripts[@]}"; do
+			echo -e "\nExecuting $bashfile ...\n"
+		    bash "$shdir/$bashfile" &
+		    pid_array+=($!)
+        done
+		for pid in "${pid_array[@]}"; do
+		    wait "$pid" || exit $?
+		done
+    else
+        echo "Invalid mode: $mode"
+        exit 1
+    fi
+}
+
+json_data=$(cat $shdir/run_order.json)
+
+for config in $(echo "$json_data" | jq -c '.[]'); do
+    mode=$(echo "$config" | jq -r '.mode')
+    scripts=($(echo "$config" | jq -r '.scripts[]'))
+
+    echo "Executing the following scripts in $mode mode:"
+    echo "Scripts: ${scripts[@]}"
+    run_scripts "$mode" $scripts
 done
