e3d1f54ff5641dc8f68e4b6363550bb09feed25b
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2024-01-20 13:46:26 +0600

    Reorder scripts

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -3,8 +3,8 @@
 set -eux
 
 echo "\nInstalling Docker ...\n"
-apt -y update
-apt -y install ca-certificates curl gnupg
+apt update -y
+apt install -y ca-certificates curl gnupg
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor --batch --yes -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
@@ -14,7 +14,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt -y update
+apt update -y
 
 apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
