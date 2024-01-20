12353f0f18c4ecdb5090153130ee177c7225b6f7
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2024-01-20 11:33:51 +0600

    Run all parallel

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
+#!/bin/bash
+
 set -eux
 
 shdir="scripts"
 
 for bashfile in $(cat $shdir/run_order.txt); do
-	echo -e "\nExecuting $bashfile ...\n"
-	bash "$shdir/$bashfile"
+    echo -e "\nExecuting $bashfile ...\n"
+    bash "$shdir/$bashfile" &
+    # Store the background process ID in an array for later checking
+    pid_array+=($!)
+done
+
+# Wait for all background processes to finish and check their exit statuses
+for pid in "${pid_array[@]}"; do
+    wait "$pid" || exit $?
 done
