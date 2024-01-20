25af7e71f8db2f77afe1468c2e1c30eb736babe5
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2024-01-20 13:57:33 +0600

    Reorder minikube

--- scripts/minikube.sh
+++ scripts/minikube.sh
@@ -4,5 +4,5 @@ set -eux
 
 echo "\nInstalling Minikube ...\n"
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
-sudo dpkg -i minikube_latest_amd64.deb
+dpkg -i minikube_latest_amd64.deb
 rm minikube_latest_amd64.deb
