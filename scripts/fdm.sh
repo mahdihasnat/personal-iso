95f20ff261d0d339cda4ab73d80bb8174de8c82e
Mahdi Hasnat Siyam <mahdi.siyam@chaldal.net>
2024-01-21 12:52:15 +0600

    Missed relative address in apt install

--- scripts/fdm.sh
+++ scripts/fdm.sh
@@ -7,7 +7,7 @@ echo "\nInstalling Free Download Manager ...\n"
 echo "Downloading Free Download Manager ..."
 wget -O fdm.deb https://files2.freedownloadmanager.org/6/latest/freedownloadmanager.deb
 
-apt install -o DPkg::Lock::Timeout=-1 -y fdm.deb
+apt install -o DPkg::Lock::Timeout=-1 -y ./fdm.deb
 
 echo "Removing fdm.deb ..."
 rm fdm.deb
