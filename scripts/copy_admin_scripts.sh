42c852ae7a567692065da1476493df56bd080b1b
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2024-01-22 22:08:01 +0600

    Debug print copy_admin_scripts.sh

--- scripts/copy_admin_scripts.sh
+++ scripts/copy_admin_scripts.sh
@@ -10,9 +10,12 @@ cd $ADMIN_SCRIPTS_DIR
 
 homedir="/home/$ADMIN_USER"
 for file in *; do
+	echo "Copying $file to $homedir ..."
 	cp -rv $file "$homedir/"
+	echo "chown -R -c -v $user:$user "$homedir/$file""
 	chown -R -c -v $user:$user "$homedir/$file"
+	echo "chmod -R -v 750 "$homedir/$file""
 	chmod -R -v 750 "$homedir/$file"
 done
-
+echo "running popd"
 popd
