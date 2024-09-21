aca0eb8c752f79e490c81e1a87b2fc7c2116684d
Mahdi Hasnat Siyam <44502454+mahdihasnat@users.noreply.github.com>
2024-09-21 18:22:24 +0600

    Use wget instead of curl in several installers (#14)

--- scripts/azuredatastudio.sh
+++ scripts/azuredatastudio.sh
@@ -5,7 +5,7 @@ echo "\nInstalling Azure Data Studio ...\n"
 
 apt-get -o DPkg::Lock::Timeout=-1 -y install libunwind8
 
-curl -L -o azuredatastudio.deb https://azuredatastudio-update.azurewebsites.net/latest/linux-deb-x64/stable
+wget -O azuredatastudio.deb https://azuredatastudio-update.azurewebsites.net/latest/linux-deb-x64/stable
 
 apt-get -o DPkg::Lock::Timeout=-1 -y install ./azuredatastudio.deb
 
